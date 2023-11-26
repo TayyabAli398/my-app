461f7f268b8fbbcfb9271d62dbc859012d518ad6
Tayyab Ali <alitayyab398@gmail.com>
2023-11-26 14:22:16 +0500

    edit hero image

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -6,14 +6,11 @@ const About = () => {
     <div className='flex flex-col py-40'>
         <h1 className='md:text-5xl text-2xl text-center font-bold py-6'>About Us</h1>
         <div className='md:flex-row flex flex-col justify-around items-center gap-6 py-10 px-20'>
-<Image src={"/Hero.jpg"} alt='About-img' width={500} height={200} className='rounded-lg shadow-lg w-auto h-auto ' />
-<div className='pl-10'>
-    <p className='md:text-2xl text-sm font-normal py-4'>Hi, I'm Tayyab Ali, a full stack web developer based in Lahore, Pakistan. I have a passion for creating beautiful and functional websites using modern technologies.I have a bachelor's degree in computer science from the University of Virtual and three years of experience working as a web developer for various companies and freelance projects. I have developed websites using HTML, CSS, JavaScript,Next.js, React, Node.js,Tailwand Css, and other tools and frameworks </p>
-    
-
-</div>
-        </div>
-
+    <Image src={"/Hero.jpg"} alt='About-img' width={500} height={200} className='rounded-lg shadow-lg w-auto h-auto ' />
+      <div className='pl-10'>
+      <p className='md:text-2xl text-sm font-normal py-4'>Hi, I'm Tayyab Ali, a full stack web developer based in Lahore, Pakistan. I have a passion for creating beautiful and functional websites using modern technologies.I have a bachelor's degree in computer science from the University of Virtual and three years of experience working as a web developer for various companies and freelance projects. I have developed websites using HTML, CSS, JavaScript,Next.js, React, Node.js,Tailwand Css, and other tools and frameworks </p>
+    </div>
+    </div>
     </div>
   )
 }
--- src/app/components/Homepage.tsx
+++ src/app/components/Homepage.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Homepage = () => {
   return (
     <div className='flex flex-col justify-center items-center pt-20 pb-24 md:px-0 px-20'>
-<Image src={"/Image.jpg"} width={300} height={300} alt='profile image' className='rounded-full'/>
+<Image src={"/HeroImage.png"} width={100} height={100} alt='profile image' className='rounded-full w-64 h-60'/>
 <h1 className='text-lg md:text-6xl font-semibold from-neutral-800 pt-8 text-center '> Hello I am Tayyab Ali</h1>
 <p className='text-sm md:text-2xl font-semibold text-center py-6'>UI/UX Designer/Developer || Full Stack Web Developer</p>
 <button className='text-lg border rounded-md py-4 px-6 hover:bg-black hover:text-white'>Join Now</button>
